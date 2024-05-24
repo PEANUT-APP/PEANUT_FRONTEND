e121709a1e84be6eada2b03f120b221a1a08ee15
jieun <122079153+Cjieun@users.noreply.github.com>
2024-05-24 16:36:13 +0900

    onBoarding UI

---
+++ src/screens/onBoarding/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components/native';
+import {Body1, Body2} from '../../components/text/Text';
+
+export const OnBoardingContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+  padding: 80px 0;
+`;
+
+export const OnBoardingLogo = styled.View`
+  width: 131px;
+  height: 131px;
+  background-color: #d9d9d9;
+  margin-top: 106px;
+`;
+
+export const OnBoardingLoginBox = styled.View`
+  gap: 8px;
+  align-items: center;
+`;
+
+export const KakaoLoginBox = styled.TouchableOpacity`
+  width: 350px;
+  height: 48px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+  background-color: #ffe812;
+  flex-direction: row;
+  gap: 7px;
+`;
+
+export const KakaoLoginText = styled(Body1)`
+  line-height: 21.344px;
+  letter-spacing: -0.4px;
+`;
+
+export const SignInBox = styled.TouchableOpacity`
+  margin-bottom: 4px;
+`;
+
+export const SignInText = styled(Body2)`
+  line-height: 18.676px;
+  letter-spacing: -0.35px;
+`;
