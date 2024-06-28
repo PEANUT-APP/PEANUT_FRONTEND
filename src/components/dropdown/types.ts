ce5975253eed9d519f783681ae9c69b278123f34
jieun <122079153+Cjieun@users.noreply.github.com>
2024-06-29 00:48:25 +0900

    DropdownField Component

---
+++ src/components/dropdown/types.ts
@@ -0,0 +1,3 @@
+export interface DropdownFieldType {
+  children: string;
+}
---
+++ src/pages/test/DropdownTest.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import DropdownField from '../../components/dropdown/DropdownField';
+
+export default function DropdownTest() {
+  return <DropdownField>이건 피넛의 드롭 다운 입니다</DropdownField>;
+}
